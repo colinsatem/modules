2bfe0bc3c4bc43a1a7434c859b1a04bb82286270
colinsatem <85325005+colinsatem@users.noreply.github.com>
2021-07-24 21:35:27 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "jjtech" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.name
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+
+}
+
+variable "instance_type" {
+
+}
+
+variable "name" {
+
+}
